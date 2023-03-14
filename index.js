c4067e1fc5fde6624cf2f2f606ae18f65375065c
Chris <chris.kamau@student.moringaschool.com>
2023-03-14 12:48:12 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply;
+multiply = num1 * num2;
+multiply = 62;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let mod = (num3 % num4 + 4) % num4
+let num3 = 6;
+let num4 = 4;
+mod = 4;
+
+let max= Math.max(11, 20, 15, 14, 5);
